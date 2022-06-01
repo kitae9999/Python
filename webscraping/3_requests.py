f3cde4a8ff438edf6716b78192c2daeb854ed923
george park <gitae9999@naver.com>
2022-06-01 22:32:58 +0900

    6

--- webscraping/3_requests.py
+++ webscraping/3_requests.py
@@ -11,6 +11,6 @@ res.raise_for_status()
 
 print(len(res.text))
 print(res.text)
-
+ 
 with open("mygoogle.html","w",encoding="utf-8") as f:
     f.write(res.text)
--- webscraping/4_re.py
+++ webscraping/4_re.py
@@ -7,10 +7,14 @@ p=re.compile("ca.e")
 # ^ (^de): 문자열의 시작 > desk, destiny | fade
 # $ (se$): 문자열의 끝 > case, base | face
 
-m=p.match("case")
-print(m.group) #매치되지 않으면 에러가 발생 zmn 
+def print_match(m):
+    if m:
+        print(m.group())#매치되지 않으면 에러가 발생 
+    else:
+        print("매칭되지않음")
+        
+# m=p.match("case") #주어진 문자열의 처음부터 일치하는지 확인
+# print_match(m)
 
-if m:
-    print(m.group())
-else:
-    print("매칭되지않음")
+m=p.search("good care") #search : 주어진 문자열 중에 일치하는게 있는지 확인
+print_match(m)
